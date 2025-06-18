cfbdf42292873881b781e98aaed5e31206838b42
Sackii <98980736+Sackii-dev@users.noreply.github.com>
2025-06-18 20:13:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const supabaseUrl = 'https://supabase.com/dashboard/project/gqjygbsbrzzrsxrzylvz'; // remplace par ton URL Supabase
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxanlnYnNicnp6cnN4cnp5bHZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyNjc0OTgsImV4cCI6MjA2NTg0MzQ5OH0.PMqPYdlA9xRHFNkuswGz5hBVulIRFtm3BudPzitCTN0'; // remplace par ta clé ANON
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 document.addEventListener("DOMContentLoaded", () => {
     const colorInput = document.getElementById("primary-color");
